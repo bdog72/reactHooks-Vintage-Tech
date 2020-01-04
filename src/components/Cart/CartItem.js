7c698f367ad086c23be9cfb5c839dad0ad274906
Brian Bycynski <high5bri@hotmail.com>
2020-01-04 02:48:16 -0500

    Finished Lecture 237

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,48 @@
-import React from "react";
+//
+//
+import React from 'react';
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 
-export default function CartItem() {
-  return <h1>hello form cart item</h1>;
+import { CartContext } from '../../context/cart';
+
+export default function CartItem({ id, image, title, price, amount }) {
+  return (
+    <article className="cart-item">
+      <img src={image} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h5>${price}</h5>
+        <button
+          type="button"
+          className="cart-btn remove-btn"
+          onClick={() => {
+            console.log('Item Removed Bozo');
+          }}
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => {
+            console.log('Amount Increased');
+          }}
+        >
+          <FaAngleUp />
+        </button>
+        <p className="item-amount">{amount}</p>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => {
+            console.log('Amount Decreased');
+          }}
+        >
+          <FaAngleDown />
+        </button>
+      </div>
+    </article>
+  );
 }
--- src/context/cart.js
+++ src/context/cart.js
@@ -8,12 +8,46 @@ function CartProvider({ children }) {
   const [cart, setCart] = React.useState(localCart);
   const [total, setTotal] = React.useState(0);
   const [cartItems, setCartItems] = React.useState(0);
+
+  React.useEffect(() => {
+    // local storage
+
+    // cart items
+    let newCartItems = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount);
+    }, 0);
+    setCartItems(newCartItems);
+
+    // cart total
+    let newTotal = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount * cartItem.price);
+    }, 0);
+    newTotal = parseFloat(newTotal.toFixed(2));
+    setTotal(newTotal);
+  }, [cart]);
+
+  // remove item
+  const removeItem = id => {};
+  // increase amount
+  const increaseAmount = id => {};
+  // decrease amount
+  const decreaseAmount = id => {};
+  // add to cart
+  const addToCart = product => {};
+  // clear cart
+  const clearCart = () => {};
+
   return (
     <CartContext.Provider
       value={{
         cart,
         total,
-        cartItems
+        cartItems,
+        removeItem,
+        increaseAmount,
+        decreaseAmount,
+        addToCart,
+        clearCart
       }}
     >
       {children}
